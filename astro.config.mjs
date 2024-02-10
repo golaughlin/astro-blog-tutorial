d95aae8624249ae1261abe76118ad9a08ab75126
Gage M. <yeen125@gmail.com>
2024-02-10 00:17:06 -0600

    Added RSS Feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://master--tangerine-taiyaki-793c73.netlify.app/"
+});
