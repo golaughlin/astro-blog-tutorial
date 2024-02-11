093944acada3fa89570f0e73ade8a89ba0823af9
Gage M. <yeen125@gmail.com>
2024-02-10 20:34:34 -0600

    Added Dark Theme and Preact Component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://master--tangerine-taiyaki-793c73.netlify.app/"
+  site: "https://master--tangerine-taiyaki-793c73.netlify.app/",
+  integrations: [preact()]
 });
